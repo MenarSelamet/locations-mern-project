c2cb22cc7ae718f4e345cba320ad02ab2348238e
Menar Selamet <menarselamet007@gmail.com>
2024-07-03 16:23:27 +0300

    Added Routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import Home from './Pages/Home/Home';
+import Places from './Pages/Places/Places';
+import Auth from './Pages/Authentication/Auth';
+import NewPlaceForm from './Pages/NewPlace/NewPlaceForm';
+import UpdatePlaceForm from './Pages/UpdatePlace/UpdatePlaceForm';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello fucking World
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/places" element={<Places />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/new" element={<NewPlaceForm />} />
+        <Route path="/update" element={<UpdatePlaceForm />} />
+      </Routes>
     </div>
   );
 }
